440f7890a48fe935d66b781c3a50302609a12148
LeadRDRK <leadrdrk@zohomail.com>
2025-12-23 17:59:03 +0700

    [MMLib 771cd04] Update IFoodLike, ItemDrinkBase, ItemFoodBase
    0999312/MMLib@771cd04e97b26b49d46f63bae1d91440055c7d6e

--- src/main/java/net/tracen/umapyoi/item/ItemFoodBase.java
+++ src/main/java/net/tracen/umapyoi/item/ItemFoodBase.java
@@ -1,25 +1,48 @@
 package net.tracen.umapyoi.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.tracen.umapyoi.item.info.FoodInfo;
 
+import java.util.List;
+
 public class ItemFoodBase extends Item implements IFoodLike {
     private final FoodInfo info;
-
+    private final FoodProperties finalFoodProperties;
     public ItemFoodBase(Item.Properties prop, FoodInfo info) {
         super(prop);
         this.info = info;
+        FoodProperties.Builder food = new FoodProperties.Builder().nutrition(getFoodInfo().getAmount())
+                .saturationMod(getFoodInfo().getCalories());
+        if (info.isAlwaysEat())
+            food.alwaysEat();
+        if (info.getEatTime() <= 16)
+            food.fast();
+        this.info.getEffects().forEach((k) -> food.effect(k.getFirst().get(), k.getSecond()));
+        this.finalFoodProperties = food.build();
     }
 
     @Override
     public boolean isEdible() {
         return this.info != null;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, Level level, List<Component> tooltips, TooltipFlag flag) {
+        super.appendHoverText(itemStack, level, tooltips, flag);
+        if(!this.shouldAddEffectTooltips())
+            return;
+
+        if(!this.getFoodInfo().getEffects().isEmpty()) {
+            this.addEffectTooltips(tooltips);
+        }
     }
 
     @Override
@@ -51,27 +74,24 @@ public class ItemFoodBase extends Item implements IFoodLike {
 
     @Override
     public FoodProperties getFoodProperties() {
-        FoodProperties.Builder food = new FoodProperties.Builder().nutrition(getFoodInfo().getAmount())
-                .saturationMod(getFoodInfo().getCalories());
-        if (getFoodInfo().isAlwaysEat())
-            food.alwaysEat();
-        if (getFoodInfo().getEatTime() <= 16)
-            food.fast();
-        this.getFoodInfo().getEffects().forEach((k) -> food.effect(k.getFirst().get(), k.getSecond()));
-
-        return food.build();
+        return this.finalFoodProperties;
     }
 
     @Override
-    public int getUseDuration(ItemStack p_77626_1_) {
+    public int getUseDuration(ItemStack stack) {
         if (this.getFoodInfo() != null)
             return this.getFoodInfo().getEatTime();
-        return super.getUseDuration(p_77626_1_);
+        return super.getUseDuration(stack);
     }
 
     @Override
     public FoodInfo getFoodInfo() {
         return info;
+    }
+
+    @Override
+    public boolean shouldAddEffectTooltips() {
+        return this.info != null;
     }
 
 }

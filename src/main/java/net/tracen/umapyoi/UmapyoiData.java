f1efd872cd78fc8232aafdfe2a5c1578c734de78
LeadRDRK <leadrdrk@zohomail.com>
2025-12-25 22:06:30 +0700

    [01557e7-52597d1] Remove SupportCardDataProvider, UmaDataProvider, UmapyoiAdvancementsDataProvider
    0999312/umapyoi@01557e7ac0ec2e56a737a81e9ba0bab6088e0df1 - 0999312/umapyoi@52597d19c2ed124e9d328d75dbff62811fbd1e01

--- src/main/java/net/tracen/umapyoi/UmapyoiData.java
+++ src/main/java/net/tracen/umapyoi/UmapyoiData.java
@@ -2,9 +2,6 @@ package net.tracen.umapyoi;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.tracen.umapyoi.data.SupportCardDataProvider;
-import net.tracen.umapyoi.data.UmaDataProvider;
-import net.tracen.umapyoi.data.UmapyoiAdvancementsDataProvider;
 import net.tracen.umapyoi.data.UmapyoiLootTableProvider;
 import net.tracen.umapyoi.data.UmapyoiRecipeProvider;
 import net.tracen.umapyoi.data.tag.UmaDataTagProvider;
@@ -16,13 +13,10 @@ public class UmapyoiData implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-        pack.addProvider(UmaDataProvider::new);
-        pack.addProvider(SupportCardDataProvider::new);
         pack.addProvider(UmapyoiBlockTagProvider::new);
         pack.addProvider(UmapyoiItemTagsProvider::new);
         pack.addProvider(UmapyoiLootTableProvider::new);
         pack.addProvider(UmaDataTagProvider::new);
         pack.addProvider(UmapyoiRecipeProvider::new);
-        pack.addProvider(UmapyoiAdvancementsDataProvider::new);
     }
 }

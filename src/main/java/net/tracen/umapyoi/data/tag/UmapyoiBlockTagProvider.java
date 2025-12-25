fd238e2b45bcfa4d808c01ec91bebade2aad6bfa
LeadRDRK <leadrdrk@zohomail.com>
2025-12-25 22:50:48 +0700

    [01557e7-52597d1] Update UmapyoiBlockTagProvider
    0999312/umapyoi@01557e7ac0ec2e56a737a81e9ba0bab6088e0df1 - 0999312/umapyoi@52597d19c2ed124e9d328d75dbff62811fbd1e01

--- src/main/java/net/tracen/umapyoi/data/tag/UmapyoiBlockTagProvider.java
+++ src/main/java/net/tracen/umapyoi/data/tag/UmapyoiBlockTagProvider.java
@@ -19,11 +19,11 @@ public class UmapyoiBlockTagProvider extends BlockTagProvider {
         getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE).add(BlockRegistry.THREE_GODDESS.get())
                 .add(BlockRegistry.SUPPORT_ALBUM_PEDESTAL.get()).add(BlockRegistry.UMA_PEDESTAL.get())
                 .add(BlockRegistry.SILVER_SUPPORT_ALBUM_PEDESTAL.get()).add(BlockRegistry.SILVER_UMA_PEDESTAL.get())
-                .add(BlockRegistry.DISASSEMBLY_BLOCK.get())
+                .add(BlockRegistry.UMA_STATUES.get())
                 .add(BlockRegistry.THREE_GODDESS_UPPER.get()).add(BlockRegistry.TRAINING_FACILITY.get());
 
-        getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.SKILL_LEARNING_TABLE.get())
-                .add(BlockRegistry.REGISTER_LECTERN.get());
+        getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.DISASSEMBLY_BLOCK.get()).add(BlockRegistry.SKILL_LEARNING_TABLE.get())
+                .add(BlockRegistry.REGISTER_LECTERN.get()).add(BlockRegistry.UMA_SELECT_BLOCK.get());
 
         getOrCreateTagBuilder(UmapyoiBlockTags.TRACK_TURF)
                 .add(Blocks.GRASS_BLOCK)

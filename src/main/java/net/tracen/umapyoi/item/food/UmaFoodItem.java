7d3b7cf8e3eb66e6519e07fa14363473488903a4
LeadRDRK <leadrdrk@zohomail.com>
2025-12-25 17:16:21 +0700

    [01557e7-52597d1] Update UmaFoodItem
    0999312/umapyoi@01557e7ac0ec2e56a737a81e9ba0bab6088e0df1 - 0999312/umapyoi@52597d19c2ed124e9d328d75dbff62811fbd1e01

--- src/main/java/net/tracen/umapyoi/item/food/UmaFoodItem.java
+++ src/main/java/net/tracen/umapyoi/item/food/UmaFoodItem.java
@@ -2,6 +2,7 @@ package net.tracen.umapyoi.item.food;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.tracen.umapyoi.Umapyoi;
@@ -15,7 +16,11 @@ public class UmaFoodItem extends ItemFoodBase {
     private final Consumer<ItemStack> consumer;
 
     public UmaFoodItem(Consumer<ItemStack> consumer, FoodInfo info) {
-        super(Umapyoi.defaultItemProperties(), info);
+        this(Umapyoi.defaultItemProperties(), consumer, info);
+    }
+
+    public UmaFoodItem(Item.Properties properties, Consumer<ItemStack> consumer, FoodInfo info) {
+        super(properties, info);
         this.consumer = consumer;
     }
 

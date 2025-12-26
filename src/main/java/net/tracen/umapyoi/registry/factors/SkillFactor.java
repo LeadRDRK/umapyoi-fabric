6c9678664c72bc8a24d5a71c28e925a54cabb516
LeadRDRK <leadrdrk@zohomail.com>
2025-12-26 20:37:08 +0700

    [94b36e0] Update SkillFactor, UniqueSkillFactor
    0999312/umapyoi@94b36e069d4dbe55aa184e6ba00ce0f3b48e66ad

--- src/main/java/net/tracen/umapyoi/registry/factors/SkillFactor.java
+++ src/main/java/net/tracen/umapyoi/registry/factors/SkillFactor.java
@@ -30,6 +30,16 @@ public class SkillFactor extends UmaFactor {
     }
 
     @Override
+    public Component getDescriptionDetail(UmaFactorStack stack) {
+        ResourceLocation skill = ResourceLocation.tryParse(stack.getOrCreateTag().getString("skill"));
+        if (skill != null && UmaSkillRegistry.REGISTRY.get().containsKey(skill)) {
+            UmaSkill result = UmaSkillRegistry.REGISTRY.get().get(skill);
+            return result.getDescriptionDetail();
+        }
+        return Component.empty();
+    }
+
+    @Override
     public Component getDescription(UmaFactorStack stack) {
         ResourceLocation skill = ResourceLocation.tryParse(stack.getOrCreateTag().getString("skill"));
         if (skill != null && UmaSkillRegistry.REGISTRY.get().containsKey(skill)) {

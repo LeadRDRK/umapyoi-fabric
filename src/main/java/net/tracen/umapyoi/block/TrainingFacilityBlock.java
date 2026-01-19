ba0223e9494de894a6a700211e1f99323cc2c8fa
LeadRDRK <leadrdrk@zohomail.com>
2026-01-19 21:36:17 +0700

    Properly implement ExtendedScreenHandlerFactory to open extended screens with custom data, remove NetworkHooks

--- src/main/java/net/tracen/umapyoi/block/TrainingFacilityBlock.java
+++ src/main/java/net/tracen/umapyoi/block/TrainingFacilityBlock.java
@@ -2,7 +2,6 @@ package net.tracen.umapyoi.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -27,7 +26,6 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.tracen.umapyoi.block.entity.BlockEntityRegistry;
 import net.tracen.umapyoi.block.entity.TrainingFacilityBlockEntity;
-import net.tracen.umapyoi.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 
@@ -100,9 +98,9 @@ public class TrainingFacilityBlock extends BaseEntityBlock {
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         if (!level.isClientSide) {
-            BlockEntity tileEntity = level.getBlockEntity(pos);
-            if (tileEntity instanceof TrainingFacilityBlockEntity blockEntity) {
-                NetworkHooks.openScreen((ServerPlayer) player, blockEntity, pos);
+            var menu = state.getMenuProvider(level, pos);
+            if (menu != null) {
+                player.openMenu(menu);
             }
         }
         return InteractionResult.SUCCESS;

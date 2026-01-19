80ce3ddea8f9d318edcc1adfa8586a57e0566b45
LeadRDRK <leadrdrk@zohomail.com>
2026-01-19 22:01:30 +0700

    Remove unused OpenScreenPacket

--- src/main/java/net/tracen/umapyoi/UmapyoiClient.java
+++ src/main/java/net/tracen/umapyoi/UmapyoiClient.java
@@ -1,14 +1,12 @@
 package net.tracen.umapyoi;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.tracen.umapyoi.client.screen.ScreensRegistry;
 import net.tracen.umapyoi.events.client.RenderArmCallback;
 import net.tracen.umapyoi.events.client.RenderPlayerCallback;
 import net.tracen.umapyoi.events.client.RenderingUmaSoulCallback;
 import net.tracen.umapyoi.events.handler.ClientEvents;
 import net.tracen.umapyoi.events.handler.ClientSetupEvents;
-import net.tracen.umapyoi.network.OpenScreenPacket;
 
 public class UmapyoiClient implements ClientModInitializer {
     @Override
@@ -26,8 +24,5 @@ public class UmapyoiClient implements ClientModInitializer {
 
         // ScreensRegistry
         ScreensRegistry.register();
-
-        // Networking
-        ClientPlayNetworking.registerGlobalReceiver(OpenScreenPacket.TYPE, OpenScreenPacket::handler);
     }
 }

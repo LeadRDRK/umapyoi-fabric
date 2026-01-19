f91f61ab00ac3e988e759e58966e8bed11bb8153
LeadRDRK <leadrdrk@zohomail.com>
2026-01-19 20:29:10 +0700

    Update and refactor creative mode tabs

---
+++ src/main/java/net/tracen/umapyoi/UmapyoiCreativeGroup.java
@@ -0,0 +1,82 @@
+package net.tracen.umapyoi;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.tracen.umapyoi.item.CreativeModeTabFiller;
+import net.tracen.umapyoi.item.ItemRegistry;
+import net.tracen.umapyoi.registry.LazyRegistrar;
+import net.tracen.umapyoi.registry.RegistryObject;
+
+public class UmapyoiCreativeGroup {
+    public static final LazyRegistrar<CreativeModeTab> CREATIVE_MODE_TABS = LazyRegistrar
+            .create(Registries.CREATIVE_MODE_TAB, Umapyoi.MODID);
+
+    public static final RegistryObject<CreativeModeTab> UMAPYOI_ITEMS = CREATIVE_MODE_TABS.register("umapyoi",
+            () -> FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ItemRegistry.HACHIMI_MID.get()))
+                    .title(Component.translatable("itemGroup.umapyoi"))
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> UMAPYOI_BLANK_SOULS = CREATIVE_MODE_TABS.register("umapyoi_blank_souls",
+            () -> FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ItemRegistry.BLANK_UMA_SOUL.get()))
+                    .title(Component.translatable("itemGroup.umapyoi.blank_souls"))
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> UMAPYOI_SOULS = CREATIVE_MODE_TABS.register("umapyoi_souls",
+            () -> FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ItemRegistry.UMA_SOUL_DISPLAY.get()))
+                    .title(Component.translatable("itemGroup.umapyoi.souls"))
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> UMAPYOI_CARDS = CREATIVE_MODE_TABS.register("umapyoi_cards",
+            () -> FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ItemRegistry.SUPPORT_CARD.get()))
+                    .title(Component.translatable("itemGroup.umapyoi.cards"))
+                    .build());
+
+    public static void register() {
+        CREATIVE_MODE_TABS.register();
+
+        ItemGroupEvents
+                .modifyEntriesEvent(ResourceKey.create(Registries.CREATIVE_MODE_TAB, UMAPYOI_ITEMS.getId()))
+                .register(entries -> {
+                    for (RegistryObject<Item> object : ItemRegistry.ITEMS.getEntries()) {
+                        if (object == ItemRegistry.BLANK_UMA_SOUL || object == ItemRegistry.UMA_SOUL
+                                || object == ItemRegistry.UMA_SOUL_DISPLAY || object == ItemRegistry.SUPPORT_CARD)
+                            continue;
+
+                        Item item = object.get();
+
+                        if (item instanceof CreativeModeTabFiller filler)
+                            filler.fillItemCategory(entries);
+                        else
+                            entries.accept(item);
+                    }
+                });
+
+        ItemGroupEvents
+                .modifyEntriesEvent(ResourceKey.create(Registries.CREATIVE_MODE_TAB, UMAPYOI_BLANK_SOULS.getId()))
+                .register(entries -> {
+                    ((CreativeModeTabFiller) ItemRegistry.BLANK_UMA_SOUL.get()).fillItemCategory(entries);
+                });
+
+        ItemGroupEvents
+                .modifyEntriesEvent(ResourceKey.create(Registries.CREATIVE_MODE_TAB, UMAPYOI_SOULS.getId()))
+                .register(entries -> {
+                    ((CreativeModeTabFiller) ItemRegistry.UMA_SOUL.get()).fillItemCategory(entries);
+                });
+
+        ItemGroupEvents
+                .modifyEntriesEvent(ResourceKey.create(Registries.CREATIVE_MODE_TAB, UMAPYOI_CARDS.getId()))
+                .register(entries -> {
+                    ((CreativeModeTabFiller) ItemRegistry.SUPPORT_CARD.get()).fillItemCategory(entries);
+                });
+    }
+}

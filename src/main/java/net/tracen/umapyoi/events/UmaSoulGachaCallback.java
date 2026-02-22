74f60fb88906e7a9c6376c720ae77e38568eb050
LeadRDRK <leadrdrk@zohomail.com>
2026-02-22 21:36:53 +0700

    [1aa9a02-49506a8] Update SilverSupportAlbumPedestalBlockEntity, add gacha events/callbacks

---
+++ src/main/java/net/tracen/umapyoi/events/UmaSoulGachaCallback.java
@@ -0,0 +1,36 @@
+package net.tracen.umapyoi.events;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Collection;
+
+public interface UmaSoulGachaCallback {
+    class Context extends GachaContext {
+        public Context(ItemStack input, Collection<ResourceLocation> fulfills, ResourceLocation target,
+                       ItemStack defaultResult, RandomSource src) {
+            super(input, fulfills, target, defaultResult, src);
+        }
+
+        @Override
+        public String getType() {
+            return "umasoul";
+        }
+    }
+
+    void callback(Context context);
+
+    Event<UmaSoulGachaCallback> EVENT = EventFactory.createArrayBacked(UmaSoulGachaCallback.class,
+            (listeners) -> (context) -> {
+                for (UmaSoulGachaCallback listener : listeners) {
+                    listener.callback(context);
+                }
+            });
+
+    static void invoke(Context context) {
+        EVENT.invoker().callback(context);
+    }
+}

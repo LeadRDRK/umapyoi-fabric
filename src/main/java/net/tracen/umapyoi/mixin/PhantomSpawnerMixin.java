48c99313d161c9c9e6f70722236524b11fccafed
LeadRDRK <leadrdrk@zohomail.com>
2026-02-28 22:37:07 +0700

    fix: [PhantomSpawnerMixin] Fix onBeforeSpawn injection target

--- src/main/java/net/tracen/umapyoi/mixin/PhantomSpawnerMixin.java
+++ src/main/java/net/tracen/umapyoi/mixin/PhantomSpawnerMixin.java
@@ -2,21 +2,25 @@ package net.tracen.umapyoi.mixin;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.PhantomSpawner;
 import net.tracen.umapyoi.events.PlayerSpawnPhantomsCallback;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Iterator;
 
 @Mixin(PhantomSpawner.class)
 public class PhantomSpawnerMixin {
-    @Inject(method = "tick", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/entity/Entity;blockPosition()Lnet/minecraft/core/BlockPos;"),
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;blockPosition()Lnet/minecraft/core/BlockPos;"),
             locals = LocalCapture.CAPTURE_FAILHARD)
     private void onBeforeSpawn(ServerLevel level, boolean spawnEnemies, boolean spawnFriendlies,
-                              CallbackInfo ci, ServerPlayer serverPlayer) {
+                               CallbackInfoReturnable<Integer> ci, RandomSource z, int zz, Iterator<?> zzz,
+                               ServerPlayer serverPlayer) {
         PlayerSpawnPhantomsCallback.invoke(serverPlayer);
     }
 }

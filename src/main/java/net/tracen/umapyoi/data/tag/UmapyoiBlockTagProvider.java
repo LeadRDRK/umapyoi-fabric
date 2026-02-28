3963720b39481ca97071c442311db4e8f5965699
LeadRDRK <leadrdrk@zohomail.com>
2026-02-28 16:54:18 +0700

    feat: Add pedestal block tags to datagen

--- src/main/java/net/tracen/umapyoi/data/tag/UmapyoiBlockTagProvider.java
+++ src/main/java/net/tracen/umapyoi/data/tag/UmapyoiBlockTagProvider.java
@@ -42,5 +42,13 @@ public class UmapyoiBlockTagProvider extends BlockTagProvider {
 
         getOrCreateTagBuilder(UmapyoiBlockTags.TRACK_SNOW)
                 .forceAddTag(BlockTags.SNOW);
+
+        getOrCreateTagBuilder(UmapyoiBlockTags.PEDESTAL_UMA)
+                .add(BlockRegistry.UMA_PEDESTAL.get())
+                .add(BlockRegistry.SILVER_UMA_PEDESTAL.get());
+
+        getOrCreateTagBuilder(UmapyoiBlockTags.PEDESTAL_SUPPORT)
+                .add(BlockRegistry.SUPPORT_ALBUM_PEDESTAL.get())
+                .add(BlockRegistry.SILVER_SUPPORT_ALBUM_PEDESTAL.get());
     }
 }

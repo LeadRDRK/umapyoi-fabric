803e0195d254f8dc8f548f6feb01ba73d8722c81
LeadRDRK <leadrdrk@zohomail.com>
2026-03-20 12:37:54 +0700

    feat: Port new code to 1.20.4

--- src/main/java/net/tracen/umapyoi/block/SilverUmaPedestalBlock.java
+++ src/main/java/net/tracen/umapyoi/block/SilverUmaPedestalBlock.java
@@ -2,7 +2,6 @@ package net.tracen.umapyoi.block;
 
 import com.mojang.serialization.MapCodec;
 
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
@@ -10,6 +9,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;

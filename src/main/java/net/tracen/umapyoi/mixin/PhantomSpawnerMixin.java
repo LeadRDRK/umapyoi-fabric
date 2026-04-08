b7555840a14b3ff92fc728030d8f160936806afb
LeadRDRK <leadrdrk@zohomail.com>
2026-04-08 11:15:58 +0700

    fix: [PhantomSpawnerMixin] tick method signature

--- src/main/java/net/tracen/umapyoi/mixin/PhantomSpawnerMixin.java
+++ src/main/java/net/tracen/umapyoi/mixin/PhantomSpawnerMixin.java
@@ -9,7 +9,7 @@ import net.tracen.umapyoi.events.PlayerSpawnPhantomsCallback;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Iterator;
@@ -18,9 +18,8 @@ import java.util.Iterator;
 public class PhantomSpawnerMixin {
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;blockPosition()Lnet/minecraft/core/BlockPos;"),
             locals = LocalCapture.CAPTURE_FAILHARD)
-    private void onBeforeSpawn(ServerLevel level, boolean spawnEnemies, boolean spawnFriendlies,
-                               CallbackInfoReturnable<Integer> ci, RandomSource z, int zz, Iterator<?> zzz,
-                               ServerPlayer serverPlayer) {
+    private void onBeforeSpawn(ServerLevel level, boolean spawnEnemies, CallbackInfo ci,
+                               RandomSource z, Iterator<?> zz, ServerPlayer serverPlayer) {
         PlayerSpawnPhantomsCallback.invoke(serverPlayer);
     }
 }

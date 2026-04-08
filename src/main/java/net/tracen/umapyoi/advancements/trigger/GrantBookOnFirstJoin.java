3dc73f0958099f9661b3c81db4a19d646bf97af7
LeadRDRK <leadrdrk@zohomail.com>
2026-04-08 10:50:01 +0700

    fix: Properly implement GrantBookOnFirstJoin trigger and codec

--- src/main/java/net/tracen/umapyoi/advancements/trigger/GrantBookOnFirstJoin.java
+++ src/main/java/net/tracen/umapyoi/advancements/trigger/GrantBookOnFirstJoin.java
@@ -1,38 +1,37 @@
 package net.tracen.umapyoi.advancements.trigger;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
-import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import net.minecraft.util.ExtraCodecs;
 import net.tracen.umapyoi.Umapyoi;
 
 import java.util.Optional;
 
+@MethodsReturnNonnullByDefault
 public class GrantBookOnFirstJoin extends SimpleCriterionTrigger<GrantBookOnFirstJoin.Instance> {
     public static final ResourceLocation ID = new ResourceLocation(Umapyoi.MODID, "grant_book_on_first_join");
 
-
     @Override
     public Codec<Instance> codec() {
-        return null;
+        return Codec.unit(Instance::new);
     }
 
-    public record Instance(Optional<ContextAwarePredicate> player) implements SimpleCriterionTrigger.SimpleInstance {
-        public static final Codec<Instance> CODEC = RecordCodecBuilder.create(instance -> instance
-                .group(ExtraCodecs.strictOptionalField(EntityPredicate.ADVANCEMENT_CODEC, "player").forGetter(Instance::player))
-                .apply(instance, Instance::new));
-
+    public static class Instance implements SimpleCriterionTrigger.SimpleInstance {
         public boolean test(ServerPlayer player) {
             return Umapyoi.CONFIG.GRANT_GUIDE_ON_FIRST_JOIN();
+        }
+
+        @Override
+        public Optional<ContextAwarePredicate> player() {
+            return Optional.empty();
         }
     }
 
